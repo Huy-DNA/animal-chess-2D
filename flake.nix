0f57e87c2a3a00a7dcf4c05abffa13e53796df4f
HuyDNA <huydo862003@gmail.com>
2025-03-29 19:45:05 +0700

    chore: init

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+          uv
+          ruff
+          python312
+          python312Packages.pygame
+          neovim
+        ];
+        shellHook = ''
+          uv sync
+        '';
+      };
+    });
+}
