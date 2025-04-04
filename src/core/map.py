7ae3b412030532188efa990088670c35095278e6
HuyDNA <huydo862003@gmail.com>
2025-04-04 19:17:53 +0700

    fix: add Position class

--- src/core/map.py
+++ src/core/map.py
@@ -11,10 +11,15 @@ class Location:
 
 
 @dataclass(frozen=True)
-class Cell:
-    location: Location
+class Position:
     x: int
     y: int
+
+
+@dataclass(frozen=True)
+class Cell:
+    location: Location
+    position: Position
 
 
 @dataclass(frozen=True)
@@ -38,16 +43,16 @@ class Map:
         res = []
         left = self[x - 1, y]
         if left:
-            res.append(Cell(left, x - 1, y))
+            res.append(Cell(left, Position(x - 1, y)))
         right = self[x + 1, y]
         if right:
-            res.append(Cell(right, x + 1, y))
+            res.append(Cell(right, Position(x + 1, y)))
         up = self[x, y - 1]
         if up:
-            res.append(Cell(up, x, y - 1))
+            res.append(Cell(up, Position(x, y - 1)))
         down = self[x, y + 1]
         if down:
-            res.append(Cell(down, x, y + 1))
+            res.append(Cell(down, Position(x, y + 1)))
         return res
 
 
