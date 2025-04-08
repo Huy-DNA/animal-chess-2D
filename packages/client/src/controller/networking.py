ee3b54a13595364d2696bc5cd3326a9bca376cd5
HuyDNA <huydo862003@gmail.com>
2025-04-08 16:29:29 +0700

    feat: test PodSixNet

--- packages/client/src/controller/networking.py
+++
@@ -1,3 +0,0 @@
-from PodSixNet.Connection import connection
-
-connection.Connect(("0.0.0.0", 8686))
---
+++ packages/client/src/index.py
@@ -0,0 +1,4 @@
+from PodSixNet.Connection import ConnectionListener
+
+connection = ConnectionListener()
+connection.Connect(("0.0.0.0", 8686))
