9e4a0e06e7adac225d97054ee3d10cc4baad74a9
HuyDNA <huydo862003@gmail.com>
2025-04-08 12:16:01 +0700

    feat: init server Controller

---
+++ src/server/controller.py
@@ -0,0 +1,16 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Controller:
+    __ip: str
+    __port: int
+    __server: HTTPServer
+
+    def __init__(self, *, ip: str = "0.0.0.0", port: int = 8686):
+        self.__ip = ip
+        self.__port = port
+        self.__server = HTTPServer((ip, port), BaseHTTPRequestHandler)
+
+    def start_server(self):
+        print(f"Server listening on {self.__ip}:{self.__port}")
+        self.__server.serve_forever()
