98663c84e2aeb82ef7cc72d995bb1079be06c0e3
HuyDNA <huydo862003@gmail.com>
2025-04-08 12:22:51 +0700

    feat: init GameClientRequestHandler

--- src/server/server.py
+++ src/server/server.py
@@ -1,7 +1,11 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
-class Server:
+class GameClientRequestHandler(BaseHTTPRequestHandler):
+    pass
+
+
+class GameServer:
     __ip: str
     __port: int
     __server: HTTPServer
@@ -9,7 +13,7 @@ class Server:
     def __init__(self, *, ip: str = "0.0.0.0", port: int = 8686):
         self.__ip = ip
         self.__port = port
-        self.__server = HTTPServer((ip, port), BaseHTTPRequestHandler)
+        self.__server = HTTPServer((ip, port), GameClientRequestHandler)
 
     def start_server(self):
         print(f"Server listening on {self.__ip}:{self.__port}")
